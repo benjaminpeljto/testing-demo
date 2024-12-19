01855263a516026aa6ad1710ce9c0f9265cee45a
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-12-19 10:16:49 +0100

    jah

--- src/test/java/ba/ibu/edu/week11/MaximizedTest.java
+++ src/test/java/ba/ibu/edu/week11/MaximizedTest.java
@@ -3,10 +3,14 @@ package ba.ibu.edu.week11;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MaximizedTest {
 
@@ -29,6 +33,23 @@ public class MaximizedTest {
         Thread.sleep(1500);
         js.executeScript("alert('Hello, World!');");
         Thread.sleep(1500);
+    }
+
+    @Test
+    public void lejla() throws InterruptedException {
+            webDriver.get("http://www.uitestingplayground.com/sampleapp");
+            Thread.sleep(1000);
+            WebElement userNameInput = webDriver.findElement(By.xpath("/html/body/section/div[@class='container']/div[@class='row'][2]/div/input[@name='UserName']"));
+            WebElement passwordInput = webDriver.findElement(By.xpath("/html/body/section/div[@class='container']/div[@class='row'][3]/div/input[@name='Password']"));
+            userNameInput.sendKeys("wrong");
+            Thread.sleep(800);
+            passwordInput.sendKeys("credentials");
+            Thread.sleep(800);
+            WebElement loginBtn = webDriver.findElement(By.xpath("//button[@id='login']"));
+            loginBtn.click();
+            Thread.sleep(2000);
+            WebElement element = webDriver.findElement(By.xpath("/html/body/section/div[@class='container']/div[@class='row'][1]/div/label[@id='loginstatus']"));
+            assertEquals("Invalid username/password", element.getText());
     }
 
     @AfterAll
